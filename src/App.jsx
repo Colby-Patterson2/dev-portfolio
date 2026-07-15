933bdb10a1efc2244026980a7f70d94c510f7e81
Colby Patterson <cpdevelopsnow@gmail.com>
2026-07-15 14:57:41 -0600

    Change page title

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Navbar } from "./layout/Navbar";
 function App() {
   return (
     <div className="min-h-screen overflow-hidden">
+      <title>CP Portfolio</title>
       <Navbar />
       <main>
         <Hero />
